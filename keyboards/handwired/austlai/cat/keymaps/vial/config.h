58d9f85dc5265c4b1bc408bb1dabb7f046ad46b6
Austin Lai <austinlai2112@gmail.com>
2024-08-11 19:44:31 +1000

    Add handwired cat keyboard

---
+++ keyboards/handwired/austlai/cat/keymaps/vial/config.h
@@ -0,0 +1,11 @@
+/* SPDX-License-Identifier: GPL-2.0-or-later */
+
+#pragma once
+
+#define VIAL_KEYBOARD_UID {0x45, 0x80, 0x60, 0x02, 0x9C, 0x68, 0x37, 0x42}
+
+#define VIAL_UNLOCK_COMBO_ROWS {0, 0}
+#define VIAL_UNLOCK_COMBO_COLS {0, 1}
+
+/* default layer count */
+#define DYNAMIC_KEYMAP_LAYER_COUNT 6
